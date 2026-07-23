5096c85d52741bd863a769bf3c485db63972fab9
Jojo Smith <277147121+JS-Design005@users.noreply.github.com>
2026-07-23 15:16:49 +1000

    Added 2 Sections, added icons

--- static/scripts/slides.js
+++ static/scripts/slides.js
@@ -1,3 +1,4 @@
+//slideshow
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -6,13 +7,23 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].classList.remove("active");
+    slides[i].style.display = "none";
   }
-  slides[slideIndex-1].classList.add("active");
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
